d077bebd51e8477cbc72009f0d70325e0ef2cc85
wizardleen <wizardleen@gmail.com>
2026-02-06 23:25:38 +0800

    chore: externalize doc site URL to environment variable
    chore: adopt new manul API for the examples

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
     readonly PUBLIC_MANUL_HOST: string
     readonly PUBLIC_MANUL_APP_ID: string
+    readonly PUBLIC_DOCS_URL: string
+    readonly PUBLIC_SLIDES_URL: string
 }
 
 interface ImportMeta {
